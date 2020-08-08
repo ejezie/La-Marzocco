a7ebceadfaee9ce1f617e236a80875e945ee39d7
anair92 <anair@pcpl.io>
2020-08-08 18:25:36 +0530

    Initial commit

--- build/js/custom.js
+++ build/js/custom.js
@@ -2488,28 +2488,19 @@ function init_DataTables() {
         if ($("#datatable-buttons").length) {
             $("#datatable-buttons").DataTable({
                 dom: "Blfrtip",
-                buttons: [
-                    {
-                        extend: "copy",
-                        className: "btn-sm"
-                    },
-                    {
-                        extend: "csv",
-                        className: "btn-sm"
-                    },
-                    {
-                        extend: "excel",
-                        className: "btn-sm"
-                    },
-                    {
-                        extend: "pdfHtml5",
-                        className: "btn-sm"
-                    },
-                    {
-                        extend: "print",
-                        className: "btn-sm"
-                    },
-                ],
+                buttons : [{
+                  extend: 'csv',
+                  // className: 'btn btn-sm btn-success',
+                  titleAttr: 'CSV export.',
+                  text: 'CSV',
+                  // filename: 'attendance',
+                  extension: '.csv'
+                }, {
+                  extend: 'pdf',
+                  // className: 'btn btn-sm btn-danger',
+                  titleAttr: 'Copy table data.',
+                  text: 'PDF'
+                }],
                 responsive: true
             });
         }
@@ -5037,3 +5028,57 @@ $(document).ready(function () {
     init_autocomplete();
 
 });	
+
+
+
+
+// Edit Datatable
+var table;
+$(document).ready(function () {
+    table = $('#datatable').DataTable();
+    table.MakeCellsEditable({
+        "onUpdate": myCallbackFunction,
+        "inputCss":'my-input-class',
+        "columns": [0,1,2,3],
+        "allowNulls": {
+            "columns": [3],
+            "errorClass": 'error'
+        },
+        "confirmationButton": { // could also be true
+            "confirmCss": 'my-confirm-class',
+            "cancelCss": 'my-cancel-class'
+        },
+        "inputTypes": [
+            {
+                "column": 0,
+                "type": "text",
+                "options": null
+            },
+            {
+                "column":1, 
+                "type": "list",
+                "options":[
+                    { "value": "1", "display": "Beaty" },
+                    { "value": "2", "display": "Doe" },
+                    { "value": "3", "display": "Dirt" }
+                ]
+            },
+            {
+                "column": 2,
+                "type": "datepicker", // requires jQuery UI: http://http://jqueryui.com/download/
+                "options": {
+                    "icon": "http://jqueryui.com/resources/demos/datepicker/images/calendar.gif" // Optional
+                }
+            }
+             // Nothing specified for column 3 so it will default to text
+            
+        ]
+    });
+
+});
+
+function myCallbackFunction(updatedCell, updatedRow, oldValue) {
+    console.log("The new value for the cell is: " + updatedCell.data());
+    console.log("The old value for that cell was: " + oldValue);
+    console.log("The values for each cell in that row are: " + updatedRow.data());
+}
