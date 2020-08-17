7716358284521affbf1760b77de6762a304c8bf9
anair92 <anair@pcpl.io>
2020-08-17 10:27:49 +0530

    1. Landing Page changes. 2. Menu bar js.

---
+++ customer/assets/js/menubar.js
@@ -0,0 +1,210 @@
+
+var categoryArr = {
+				    Categories: [
+				        {
+				            "name": "Commercial Machines",
+				            "family": [{
+				                    "name" : "Linea",
+				                    "type" : [  
+				                                {
+				                                    "name" : "linea pb x"
+				                                },
+				                                {
+				                                    "name" : "linea pb"
+				                                },
+				                                {
+				                                    "name" : "linea classic"
+				                                }
+
+				                    ]
+				                },
+				                {
+				                    "name" : "Leva",
+				                    "type" : [  
+				                                {
+				                                    "name" : "leva x"
+				                                },
+				                                {
+				                                    "name" : "leva s"
+				                                }
+				                    ]
+				                },
+				                {
+				                    "name" : "Strada",
+				                    "type" : [  
+				                                {
+				                                    "name" : "strada av"
+				                                },
+				                                {
+				                                    "name" : "strada ep"
+				                                }
+				                    ]
+				                },
+				                {
+				                    "name" : "Strada",
+				                    "type" : [  
+				                                {
+				                                    "name" : "strada av"
+				                                },
+				                                {
+				                                    "name" : "strada ep"
+				                                }
+				                    ]
+				                }
+				            ],
+				        },
+				        {
+				            "name": "Grinders",
+				            "family": [{
+				                    "name" : "Grinders",
+				                    "type" : [  
+				                                {
+				                                    "name" : "Swift"
+				                                },
+				                                {
+				                                    "name" : "Vulcano"
+				                                },
+				                                {
+				                                    "name" : "Lux d"
+				                                }
+
+				                    ]
+				                },
+				                
+				            ],
+				        },
+				        {
+				            "name": "Modbar",
+				            "family": [{
+				                    "name" : "Modbar",
+				                    "type" : [  
+				                                {
+				                                    "name" : "Modbar 1"
+				                                },
+				                                {
+				                                    "name" : "Modbar 2"
+				                                },
+				                                {
+				                                    "name" : "Modbar 3"
+				                                }
+
+				                    ]
+				                },
+				                
+				            ],
+				        },
+				    ]
+				}
+
+
+function showMenubarCommercialMachine(category){
+	var menuBarHTML = ""
+	menuBarHTML += '<ul class="mega_menu_inner">'
+
+	for(i=0; i< categoryArr["Categories"].length; i++){
+		categoryName = categoryArr["Categories"][i]["name"]
+		if(categoryName == category){
+			for(j=0; j < categoryArr["Categories"][i]["family"].length; j++){
+				var family = categoryArr["Categories"][i]["family"][j]["name"]
+				menuBarHTML += '<li><a href="#">'+family+'</a>'
+				menuBarHTML += '<ul>'
+
+
+				for(k=0; k< categoryArr["Categories"][i]["family"][j]["type"].length ; k++){
+					var type = categoryArr["Categories"][i]["family"][j]["type"][k]["name"]
+					menuBarHTML += '<li><a href="#">'+type+'</a></li>'
+				}
+
+				menuBarHTML += '</ul>'
+				menuBarHTML += '</li>'
+
+			}
+		}
+	}
+
+	menuBarHTML += '</ul>';
+	// Image
+	menuBarHTML += '<div class="banner_static_menu">'
+	menuBarHTML += '<a href="shop.html"><img src="https://au.lamarzocco.com/wp-content/uploads/2018/06/Leva-X-1.jpg" alt="" style="height: 400px ; width: 100%"></a>'
+	menuBarHTML += '</div>'
+
+	$("#commercial_machine_menu").append(menuBarHTML)
+}
+
+
+function showMenubarGrinders(category){
+	var menuBarHTML = ""
+	menuBarHTML += '<ul class="mega_menu_inner">'
+
+	for(i=0; i< categoryArr["Categories"].length; i++){
+		categoryName = categoryArr["Categories"][i]["name"]
+		if(categoryName == category){
+			for(j=0; j < categoryArr["Categories"][i]["family"].length; j++){
+				var family = categoryArr["Categories"][i]["family"][j]["name"]
+				menuBarHTML += '<li><a href="#">'+family+'</a>'
+				menuBarHTML += '<ul>'
+
+
+				for(k=0; k< categoryArr["Categories"][i]["family"][j]["type"].length ; k++){
+
+					var type = categoryArr["Categories"][i]["family"][j]["type"][k]["name"]
+					menuBarHTML += '<li><a href="#">'+type+'</a></li>'
+				}
+
+				menuBarHTML += '</ul>'
+				menuBarHTML += '</li>'
+
+			}
+		}
+	}
+
+	menuBarHTML += '</ul>';
+	// Image
+	menuBarHTML += '<div class="banner_static_menu">'
+	menuBarHTML += '<a href="shop.html"><img src="https://au.lamarzocco.com/wp-content/uploads/2018/06/Leva-X-1.jpg" alt="" style="height: 400px ; width: 100%"></a>'
+	menuBarHTML += '</div>'
+
+	$("#grinders_menu").append(menuBarHTML)
+}
+
+
+function showMenubarModbar(category){
+	var menuBarHTML = ""
+	menuBarHTML += '<ul class="mega_menu_inner">'
+
+	for(i=0; i< categoryArr["Categories"].length; i++){
+		categoryName = categoryArr["Categories"][i]["name"]
+		if(categoryName == category){
+			for(j=0; j < categoryArr["Categories"][i]["family"].length; j++){
+				var family = categoryArr["Categories"][i]["family"][j]["name"]
+				menuBarHTML += '<li><a href="#">'+family+'</a>'
+				menuBarHTML += '<ul>'
+
+
+				for(k=0; k< categoryArr["Categories"][i]["family"][j]["type"].length ; k++){
+
+					var type = categoryArr["Categories"][i]["family"][j]["type"][k]["name"]
+					menuBarHTML += '<li><a href="#">'+type+'</a></li>'
+				}
+
+				menuBarHTML += '</ul>'
+				menuBarHTML += '</li>'
+
+			}
+		}
+	}
+
+	menuBarHTML += '</ul>';
+	// Image
+	menuBarHTML += '<div class="banner_static_menu">'
+	menuBarHTML += '<a href="shop.html"><img src="https://au.lamarzocco.com/wp-content/uploads/2018/06/Leva-X-1.jpg" alt="" style="height: 400px ; width: 100%"></a>'
+	menuBarHTML += '</div>'
+
+	$("#modbar_menu").append(menuBarHTML)
+}
+
+showMenubarCommercialMachine("Commercial Machines")
+showMenubarGrinders("Grinders")
+showMenubarModbar("Modbar")
+
+
