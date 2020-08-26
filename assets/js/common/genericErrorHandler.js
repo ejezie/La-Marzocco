061855cbdcdb4cf4458f06636bdfe90e2eab791e
ONEANDONLYOPUS <tenkaleomkar7@gmail.com>
2020-08-26 17:06:17 +0530

    Fix logout redirection
    Show multiple errors combined

--- assets/js/common/genericErrorHandler.js
+++ assets/js/common/genericErrorHandler.js
@@ -11,11 +11,20 @@ function errorResponseHandler(error) {
              // window.location.pathname   = "/login.html";
         }else if(error.response.data.message){
                 if(typeof error.response.data.message === 'string' || error.response.data.message instanceof String){
-                 notifyError(error.response.data.message);
+                    notifyError(error.response.data.message);
                 }else if(typeof error.response.data.message === 'object'){
+                    var longMessage="";
                     for (var key of Object.keys(error.response.data.message)) {
-                        notifyError(error.response.data.message[key][0]);
+                        longMessage+=(error.response.data.message[key][0]);
+                        longMessage+="\n\n";
                     }
+
+                    new Notyf().error({
+                      message: longMessage,
+                      duration: 60000,
+                      icon: false,
+                      dismissible: true
+                    })
                 }
             }else{
              notifyError("Something went wrong...");
