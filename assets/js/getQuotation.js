b3f5393a711f509da8cc5f6aa510d08ee3848be0
Akash Nair <anair@pcpl.io>
2020-09-29 15:56:37 +0530

    Get Quotation Updated

--- assets/js/getQuotation.js
+++ assets/js/getQuotation.js
@@ -1,58 +1,154 @@
-var quotationItem = [
-					{
-						"productName" : "Leva s",
-						"productQuantity" : "1",
-						"productPrice" : "$1000"
+var orderArr = [
+					{	
+						"orderId" : "ord10012020",
+						"date" : "10th Jan 2020",
+						"totalQuantity" : "2",
+						"totalPrice" : "$1900",
+						"status" : "Delivered",
+						"orderDetails" : [
+											{
+												"productId" : "levas123",
+												"productImage" : "",
+												"productName" : "Leva s",
+												"productQuantity" : "1",
+												"productPrice" : "$900.00",
+												"totalProductPrice" : "$900.00",
+												"status" : "Delivered"
+
+											},
+											{
+												"productId" : "levar123",
+												"productImage" : "",
+												"productName" : "Leva r",
+												"productQuantity" : "1",
+												"productPrice" : "$1000.00",
+												"totalProductPrice" : "$1000.00",
+												"status" : "In Transit"
+
+											}
+										]
 					},
 					{
-						"productName" : "Linea Mini",
-						"productQuantity" : "1",
-						"productPrice" : "$980"
-					}
+						"orderId" : "ord20032020",
+						"date" : "20th Mar 2020",
+						"totalQuantity" : "2",
+						"totalPrice" : "$1700",
+						"status" : "In Transit",
+						"orderDetails" : [
+											{
+												"productId" : "lineamini123",
+												"productImage" : "",
+												"productName" : "Linea Mini",
+												"productQuantity" : "1",
+												"productPrice" : "$900.00",
+												"totalProductPrice" : "$900.00",
+												"status" : "In Transit"
+
+											},
+											{
+												"productId" : "modbar123",
+												"productImage" : "",
+												"productName" : "Modbar",
+												"productQuantity" : "1",
+												"productPrice" : "$800.00",
+												"totalProductPrice" : "$800.00",
+												"status" : "Delivered"
+
+											}
+										]
+					},
 				]
 
 
 
 
+async function showCart(orderArr){
 
-async function showCart(quotationItem){
-
-	var quotationHTML = ""
+	var myOrdersHTML = ""
 
 
-	quotationHTML += '<table>'
-	quotationHTML += '<thead>'
-	quotationHTML += '<tr>'
-	quotationHTML += '<th class="product_name">Product</th>'
-	quotationHTML += '<th class="product_thumb">Name</th>'
-	quotationHTML += '<th class="product-price">Price</th>'
-	quotationHTML += '<th class="product_quantity">Quantity</th>'
-	// quotationHTML += '<th class="product_remove">Delete</th>'
-	quotationHTML += '<th class="product_total">Total</th>'
-	quotationHTML += '</tr>'
-	quotationHTML += '</thead>'
-	quotationHTML += '<tbody>'
+	myOrdersHTML += '<table>'
+	myOrdersHTML += '<thead>'
+	myOrdersHTML += '<tr>'
+	myOrdersHTML += '<th class="product_name">Date</th>'
+	myOrdersHTML += '<th >Items</th>'
+	myOrdersHTML += '<th class="product_quantity">Quantity</th>'
+	myOrdersHTML += '<th class="product-price">Price</th>'
+	// myOrdersHTML += '<th >Status</th>'
+	myOrdersHTML += '</tr>'
+	myOrdersHTML += '</thead>'
+	myOrdersHTML += '<tbody>'
 
-	for(i=0;i<quotationItem.length;i++){
-		// alert(JSON.stringify(cartItems[i],null,2);
-		// console.log(cartItems[i]);
-		quotationHTML += '<tr>'
-		quotationHTML += '<td class="product_thumb"><a href="#"><img src="assets/img/s-product/product.jpg" alt=""></a></td>'
-		quotationHTML += '<td class="product_name"><a href="#">'+quotationItem[i]["productName"]+'</a></td>'
-		quotationHTML += '<td class="product-price">'+quotationItem[i]["productPrice"]+'</td>'
-		quotationHTML += '<td class="product_quantity"><label>Quantity</label> <input onchange="editQuantity('+quotationItem[i]["productId"]+',this)" min="1" max="100" value="'+quotationItem[i]["productQuantity"]+'" type="number"><a ><i onclick="deleteItem('+quotationItem[i]["productId"]+')" class="fa fa-trash-o" style="width: 30px;font-size:17px"></i></a></td>'
-		// quotationHTML += '<td class="product_remove"><a href="#"><i class="fa fa-trash-o"></i></a></td>'
-		quotationHTML += '<td class="product_total">'+quotationItem[i]["productPrice"]+'</td>'
-		quotationHTML += '</tr>'
+	for(i=0;i<orderArr.length;i++){
+
+		myOrdersHTML += '<tr>'
+		myOrdersHTML += '<td >'+orderArr[i]["date"]+'</td>'
+		myOrdersHTML += '<td><a class="primary" data-toggle="modal" data-target="#modal_box" id='+orderArr[i]["orderId"]+' onclick="showOrderDetails(`'+orderArr[i]["orderId"]+'`)">View Details</a></td>'
+		myOrdersHTML += '<td>'+orderArr[i]["totalPrice"]+'</td>'
+		myOrdersHTML += '<td class="product_total">'+orderArr[i]["totalPrice"]+'</td>'
+		// myOrdersHTML += '<td>'+orderArr[i]["status"]+'</td>'
+		myOrdersHTML += '</tr>'
+
+
 	}
 
-	quotationHTML += '</tbody>'
-	quotationHTML += '</table>'
+	myOrdersHTML += '</tbody>'
+	myOrdersHTML += '</table>'
 
-	$("#quotation").append(quotationHTML)
+	$("#myOrders").append(myOrdersHTML)
 }
 
-showCart(quotationItem)
+
+async function showOrderDetails(orderId){
+
+	var orderDetails = orderArr.filter(function(order){return order.orderId == orderId;});
+
+
+	console.log("orderDetails : ",orderDetails[0]["orderDetails"])
+
+
+	var orderDetailsHTML = ""
+	$("#orderDetails").empty()
+
+
+	orderDetailsHTML += '<table>'
+	orderDetailsHTML += '<thead>'
+	orderDetailsHTML += '<tr>'
+	orderDetailsHTML += '<th class="product_thumb">Product</th>'
+	orderDetailsHTML += '<th class="product_name">Name</th>'
+	orderDetailsHTML += '<th class="product-price">Price</th>'
+	orderDetailsHTML += '<th class="product_quantity">Quantity</th>'
+	orderDetailsHTML += '<th class="product_total">Total</th>'
+	orderDetailsHTML += '<th class="product_status">Status</th>'
+	orderDetailsHTML += '<th></th>'
+	orderDetailsHTML += '</tr>'
+	orderDetailsHTML += '</thead>'
+	orderDetailsHTML += '<tbody>'
+
+
+	for(i=0;i<orderDetails[0]["orderDetails"].length;i++){
+
+		orderDetailsHTML += '<tr>'
+		orderDetailsHTML += '<td class="product_thumb"><a href="#"><img src="assets/img/s-product/product.jpg" alt=""></a></td>'
+		orderDetailsHTML += '<td class="product_name"><a href="#">'+orderDetails[0]["orderDetails"][i]["productName"]+'</a></td>'
+		orderDetailsHTML += '<td class="product-price">'+orderDetails[0]["orderDetails"][i]["productPrice"]+'</td>'
+		orderDetailsHTML += '<td class="product_quantity">'+orderDetails[0]["orderDetails"][i]["productQuantity"]+'</td>'
+		orderDetailsHTML += '<td class="product_total">'+orderDetails[0]["orderDetails"][i]["totalProductPrice"]+'</td>'
+		orderDetailsHTML += '<td class="product_total">'+orderDetails[0]["orderDetails"][i]["status"]+'</td>'
+		orderDetailsHTML += '<td><a>Add to cart</a></td>'
+		orderDetailsHTML += '</tr>'
+	}
+
+
+	orderDetailsHTML += '</tbody>'
+	orderDetailsHTML += '</table>'
+
+
+    $("#orderDetails").append(orderDetailsHTML)
+
+}
 
 
 
+
+showCart(orderArr)
